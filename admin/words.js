1d9db098a7a7659f6b54950625a9269ff4264357
Rene <info@rg-engineering.eu>
2018-01-07 18:42:30 +0100

    translations updated

--- admin/words.js
+++ admin/words.js
@@ -1,11 +1,72 @@
-/*global systemDictionary:true */
+﻿/*global systemDictionary:true */
 'use strict';
 
 systemDictionary = {
-    "DasWetter.com adapter settings": {              "en": "DasWetter.com adapter settings",                  "de": "DasWetter.com Adapter Einstellungen",             "ru": "????????? ???????? DasWetter.com",               "pt": "Configura��es do adaptador DasWetter.com",       "nl": "DasWetter.com adapterinstellingen",              "fr": "Param�tres de l'adaptateur DasWetter.com",       "it": "Impostazioni della scheda DasWetter.com",        "es": "Configuraci�n del adaptador DasWetter.com",      "pl": "Ustawienia adaptera DasWetter.com"},
-    "URL 5 days forecast": {                         "en": "URL 5 days forecast",                             "de": "URL 5-Tage-Wettervorhersage",                     "ru": "URL ???????? ?? 5 ????",                         "pt": "Previs�o de 5 dias",                             "nl": "URL 5 dagen voorspelling",                       "fr": "URL 5 jours de pr�vision",                       "it": "URL 5 giorni di previsioni",                     "es": "Pron�stico de URL de 5 d�as",                    "pl": "Prognoza adresu URL 5 dni"},
-    "URL 7 days forecast": {                         "en": "URL 7 days forecast",                             "de": "URL 7-Tage-Wettervorhersage",                     "ru": "??????? URL ?? 7 ????",                          "pt": "Previs�o de 7 dias",                             "nl": "URL 7 dagen voorspelling",                       "fr": "URL 7 jours de pr�vision",                       "it": "URL previsione di 7 giorni",                     "es": "Pron�stico de URL de 7 d�as",                    "pl": "Prognoza adresu URL na 7 dni"},
-    "URL hourly forecast": {                         "en": "URL hourly forecast",                             "de": "URL stündlichen Wettervorhersagen",               "ru": "URL-????? ????????",                             "pt": "Previs�o hor�ria de URL",                        "nl": "URL-uurprognose",                                "fr": "Pr�vision horaire de l'URL",                     "it": "Previsione oraria dell'URL",                     "es": "Pron�stico por hora de URL",                     "pl": "Prognoza godzinowa adresu URL"},
-    "on save adapter restarts with new config immediately": {"en": "on save adapter restarts with new config immediately", "de": "Beim Speichern von Einstellungen wird der Adapter sofort neu gestartet.", "ru": "?? ???????????????? ???????? ??????????????? ? ????? ????????????? ??????????", "pt": "no adaptador de salvar reinicia com nova configura��o imediatamente", "nl": "on save-adapter wordt onmiddellijk opnieuw opgestart met nieuwe config", "fr": "sur l'adaptateur de sauvegarde red�marre avec la nouvelle config imm�diatement", "it": "su save adapter si riavvia immediatamente con la nuova configurazione", "es": "en el adaptador de guardar se reinicia con nueva configuraci�n de inmediato", "pl": "na karcie save natychmiast uruchamia si? z now? konfiguracj?"},
-    "use only the URL you need, leave the others blank": {"en": "use only the URL you need, leave the others blank", "de": "nur die URL verwenden, die benötigt wird, die anderen können leer bleiben", "ru": "??????????? ?????? URL-?????, ??????? ??? ?????, ???????? ????????? ??????", "pt": "use apenas o URL que voc� precisa, deixe os outros em branco", "nl": "gebruik alleen de URL die u nodig hebt, laat de anderen leeg", "fr": "n'utilisez que l'URL dont vous avez besoin, laissez les autres vides", "it": "usa solo l'URL che ti serve, lascia gli altri vuoti", "es": "use solo la URL que necesita, deje los otros en blanco", "pl": "u?ywaj tylko potrzebnego adresu URL, pozostawiaj?c pozosta?e puste"},
+    "DasWetter.com_adapter_settings":
+    {
+        "en": "DasWetter.com adapter settings",
+        "de": "DasWetter.com Adapter Einstellungen",
+        "ru": "Настройки адаптера DasWetter.com",
+        "pt": "Configurações do adaptador DasWetter.com",
+        "nl": "DasWetter.com adapterinstellingen",
+        "fr": "Paramètres de l'adaptateur DasWetter.com",
+        "it": "Impostazioni della scheda DasWetter.com",
+        "es": "Configuración del adaptador DasWetter.com",
+        "pl": "Ustawienia adaptera DasWetter.com"
+    },
+    "URL_5_days_forecast": {
+        "en": "URL 5 days forecast",
+        "de": "URL 5-Tage-Wettervorhersage",
+        "ru": "URL прогноза на 5 дней",
+        "pt": "Previsão de 5 dias",
+        "nl": "URL 5 dagen voorspelling",
+        "fr": "URL 5 jours de prévision",
+        "it": "URL 5 giorni di previsioni",
+        "es": "Pronóstico de URL de 5 días",
+        "pl": "Prognoza adresu URL 5 dni"
+    },
+    "URL_7_days_forecast": {
+        "en": "URL 7 days forecast",
+        "de": "URL 7-Tage-Wettervorhersage",
+        "ru": "URL прогноза на 7 дней",
+        "pt": "Previsão de 7 dias",
+        "nl": "URL 7 dagen voorspelling",
+        "fr": "URL 7 jours de prévision",
+        "it": "URL previsione di 7 giorni",
+        "es": "Pronóstico de URL de 7 días",
+        "pl": "Prognoza adresu URL na 7 dni"
+    },
+    "URL_hourly_forecast": {
+        "en": "URL hourly forecast",
+        "de": "URL stündlichen Wettervorhersagen",
+        "ru": "URL-адрес ежечасно",
+        "pt": "Previsão horária de URL",
+        "nl": "URL-uurprognose",
+        "fr": "Prévision horaire de l'URL",
+        "it": "Previsione oraria dell'URL",
+        "es": "Pronóstico por hora de URL",
+        "pl": "Prognoza godzinowa adresu URL"
+    },
+    "restart_hint": {
+        "en": "on save adapter restarts with new config immediately",
+        "de": "Beim Speichern von Einstellungen wird der Adapter sofort neu gestartet.",
+        "ru": "на перезаписываемом адаптере перезагружается с новой конфигурацией немедленно",
+        "pt": "no adaptador de salvar reinicia com nova configuração imediatamente",
+        "nl": "on save-adapter wordt onmiddellijk opnieuw opgestart met nieuwe config",
+        "fr": "sur l'adaptateur de sauvegarde redémarre avec la nouvelle config immédiatement",
+        "it": "su save adapter si riavvia immediatamente con la nuova configurazione",
+        "es": "en el adaptador de guardar se reinicia con nueva configuración de inmediato",
+        "pl": "na karcie save natychmiast uruchamia si? z now? konfiguracj?"
+    },
+    "use_only_the_URL_you_need": {
+        "en": "use only the URL you need, leave the others blank",
+        "de": "nur die URL verwenden, die benötigt wird, die anderen können leer bleiben",
+        "ru": "используйте только URL-адрес, который вам нужен, оставьте остальные пустым",
+        "pt": "use apenas o URL que você precisa, deixe os outros em branco",
+        "nl": "gebruik alleen de URL die u nodig hebt, laat de anderen leeg",
+        "fr": "n'utilisez que l'URL dont vous avez besoin, laissez les autres vides",
+        "it": "usa solo l'URL che ti serve, lascia gli altri vuoti",
+        "es": "use solo la URL que necesita, deje los otros en blanco",
+        "pl": "u?ywaj tylko potrzebnego adresu URL, pozostawiaj?c pozosta?e puste"
+    },
 };
