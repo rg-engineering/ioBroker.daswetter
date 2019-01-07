9962858e5ab69114cddcb7e35874f93f585ee550
rg-engineering <info@rg-engineering.eu>
2019-01-07 19:14:00 +0100

    bug fix: check w or w/o compact mode

--- main.js
+++ main.js
@@ -2940,10 +2940,10 @@ function checkWeatherVariablesOld() {
 
 
 // If started as allInOne/compact mode => return function to create instance
-if (typeof module !== undefined && module.parent) {
+if (module && module.parent) {
     module.exports = startAdapter;
 } else {
     // or start the instance directly
     startAdapter();
-}
+} 
 
